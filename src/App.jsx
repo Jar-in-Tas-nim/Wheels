7180fc8ba32adfa015cf89dc0fe39f7a5562f8ae
Jar-in-Tas-nim <jtasnim7602@gmail.com>
2025-08-21 00:36:49 +0600

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import wheels from './components/wheels'
+
+const App = () => {
+  return (
+    <div className=''>
+      <wheels />
+    </div>
+  )
+}
+
+export default App
